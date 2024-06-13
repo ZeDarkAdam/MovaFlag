b609e91f62da575c5f92105d3d67af86ff8ed33f
Oleh <olehivaniuk.net@gmail.com>
2024-06-13 12:15:12 +0300

    Rounded Flags

--- main.cpp
+++ main.cpp
@@ -8,8 +8,8 @@ DWORD       g_layout = 0;
 
 void CALLBACK UpdateTimer(HWND hWnd, UINT uMsg, UINT_PTR idEvent, DWORD dwTime)
 {
-    int layout = (int)GetKeyboardLayout(GetWindowThreadProcessId(GetForegroundWindow(), NULL)) & 0xFFFF;
-    int caps = GetKeyState(VK_CAPITAL) & 0xFFFF;
+    int layout = (int)GetKeyboardLayout(GetWindowThreadProcessId(GetForegroundWindow(), NULL)) & 0xFFFF; // Отримання поточної мови введення
+    int caps = GetKeyState(VK_CAPITAL) & 0xFFFF; // Перевірка стану Caps Lock
    
     if (caps) {
         HCURSOR hc_new = LoadCursor(g_instance, MAKEINTRESOURCE((layout * 10) + 2));
@@ -42,14 +42,21 @@ void CALLBACK UpdateTimer(HWND hWnd, UINT uMsg, UINT_PTR idEvent, DWORD dwTime)
 int Main()
 {
     HANDLE mutex = CreateMutex(NULL, FALSE, "LangCursor");
-    if (GetLastError() == ERROR_ALREADY_EXISTS || GetLastError() == ERROR_ACCESS_DENIED) return 1;
+    if (GetLastError() == ERROR_ALREADY_EXISTS || GetLastError() == ERROR_ACCESS_DENIED) 
+        return 1;
 
+    // Виклик функції LoadCursor завантажує системний курсор з ідентифікатором IDC_IBEAM (текстовий курсор) 
+    // і копіює його, використовуючи CopyCursor. Отриманий дескриптор копіюється в глобальну змінну g_hc_ibeam
     g_hc_ibeam = CopyCursor(LoadCursor(NULL, IDC_IBEAM));
     if (!g_hc_ibeam) return 1;
 
+    // Отримання дескриптора поточного екземпляру програми
     g_instance = GetModuleHandle(NULL);
+
     g_timer = SetTimer(NULL, g_timer, 200, UpdateTimer);
-    if (!g_timer) return 1;
+
+    if (!g_timer) 
+        return 1;
 
     MSG msg;
     while (GetMessage(&msg, 0, 0, 0))
