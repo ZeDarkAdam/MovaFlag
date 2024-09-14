8a17fb032d99dfd24f0bbc72d05dac9076319dfb
Oleh <olehivaniuk.net@gmail.com>
2024-09-14 18:11:15 +0300

    Added the ability to turn off the flags for the pointer and the text cursor

--- main.cpp
+++ main.cpp
@@ -13,6 +13,25 @@ HMENU       g_hMenu;
 
 int         g_layout = 0;
 
+BOOL g_disable_arrow = FALSE;
+BOOL g_disable_ibeam = FALSE;
+
+
+//void LoadMenuStrings()
+//{
+//    TCHAR buffer[256];
+//
+//    LoadString(g_instance, ID_TRAY_DISABLE_ARROW, buffer, ARRAYSIZE(buffer));
+//    AppendMenu(g_hMenu, MF_STRING | MF_UNCHECKED, ID_TRAY_DISABLE_ARROW, buffer);
+//
+//    LoadString(g_instance, ID_TRAY_DISABLE_IBEAM, buffer, ARRAYSIZE(buffer));
+//    AppendMenu(g_hMenu, MF_STRING | MF_UNCHECKED, ID_TRAY_DISABLE_IBEAM, buffer);
+//
+//    LoadString(g_instance, ID_TRAY_EXIT, buffer, ARRAYSIZE(buffer));
+//    AppendMenu(g_hMenu, MF_STRING, ID_TRAY_EXIT, buffer);
+//}
+
+
 void CALLBACK UpdateTimer(HWND hWnd, UINT uMsg, UINT_PTR idEvent, DWORD dwTime)
 {
     int layout = (int)GetKeyboardLayout(GetWindowThreadProcessId(GetForegroundWindow(), NULL)) & 0xFFFF;
@@ -36,27 +55,29 @@ void CALLBACK UpdateTimer(HWND hWnd, UINT uMsg, UINT_PTR idEvent, DWORD dwTime)
     }*/
 
     if (g_layout != layout)
-    {
-        HCURSOR hc_new = LoadCursor(g_instance, MAKEINTRESOURCE(layout));
+    { 
+        if (!g_disable_arrow)
+        {
+            HCURSOR hc_new_arrow = LoadCursor(g_instance, MAKEINTRESOURCE(layout + 100));
 
-        if (hc_new)
-            SetSystemCursor(hc_new, OCR_IBEAM);
-        else
-            SetSystemCursor(CopyCursor(g_hc_ibeam), OCR_IBEAM);
+            if (hc_new_arrow)
+                SetSystemCursor(hc_new_arrow, OCR_NORMAL);
+            else
+                SetSystemCursor(CopyCursor(g_hc_arrow), OCR_NORMAL);
+        }
 
+        if (!g_disable_ibeam) 
+        {
+            HCURSOR hc_new_beam = LoadCursor(g_instance, MAKEINTRESOURCE(layout));
 
-
-        HCURSOR hc_new_arrow = LoadCursor(g_instance, MAKEINTRESOURCE(layout + 100));
-
-        if (hc_new_arrow)
-            SetSystemCursor(hc_new_arrow, OCR_NORMAL);
-        else
-            SetSystemCursor(CopyCursor(g_hc_arrow), OCR_NORMAL);
-
+            if (hc_new_beam)
+                SetSystemCursor(hc_new_beam, OCR_IBEAM);
+            else
+                SetSystemCursor(CopyCursor(g_hc_ibeam), OCR_IBEAM);
+        }
+        
         g_layout = layout;
     }
-
-    //g_layout = layout;
 }
 
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
@@ -64,15 +85,36 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
     switch (uMsg)
     {
     case WM_COMMAND:
-        if (LOWORD(wParam) == ID_TRAY_EXIT)
+
+        switch (LOWORD(wParam))
         {
+        case ID_TRAY_EXIT:
             PostQuitMessage(0);
+            break;
+        case ID_TRAY_DISABLE_IBEAM:
+            g_disable_ibeam = !g_disable_ibeam;
+            CheckMenuItem(g_hMenu, ID_TRAY_DISABLE_IBEAM, g_disable_ibeam ? MF_CHECKED : MF_UNCHECKED);
+
+            SetSystemCursor(CopyCursor(g_hc_ibeam), OCR_IBEAM);
+            g_layout = 0;
+
+            break;
+        case ID_TRAY_DISABLE_ARROW:
+            g_disable_arrow = !g_disable_arrow;
+            CheckMenuItem(g_hMenu, ID_TRAY_DISABLE_ARROW, g_disable_arrow ? MF_CHECKED : MF_UNCHECKED);
+
+            SetSystemCursor(CopyCursor(g_hc_arrow), OCR_NORMAL);
+            g_layout = 0;
+            
+            break;
         }
         break;
+
     case WM_DESTROY:
         Shell_NotifyIcon(NIM_DELETE, &g_notifyIconData);
         PostQuitMessage(0);
         break;
+
     case WM_USER + 1:
         if (lParam == WM_RBUTTONUP)
         {
@@ -88,24 +130,59 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
     return 0;
 }
 
+
+void SaveSettings()
+{
+    HKEY hKey;
+    // Open or create a key in the registry
+    if (RegCreateKeyEx(HKEY_CURRENT_USER, "Software\\MovaFlag\\Settings", 0, NULL, 0, KEY_WRITE, NULL, &hKey, NULL) == ERROR_SUCCESS)
+    {
+        // Save the g_disable_ibeam value
+        DWORD disableIbeam = g_disable_ibeam;
+        RegSetValueEx(hKey, "DisableIbeam", 0, REG_DWORD, (const BYTE*)&disableIbeam, sizeof(disableIbeam));
+
+        // Save the g_disable_arrow value
+        DWORD disableArrow = g_disable_arrow;
+        RegSetValueEx(hKey, "DisableArrow", 0, REG_DWORD, (const BYTE*)&disableArrow, sizeof(disableArrow));
+
+        // Close the key
+        RegCloseKey(hKey);
+    }
+}
+
+void LoadSettings()
+{
+    HKEY hKey;
+    DWORD disableIbeam = 0;
+    DWORD disableArrow = 0;
+    DWORD size = sizeof(DWORD);
+
+    // open the key in the registry
+    if (RegOpenKeyEx(HKEY_CURRENT_USER, "Software\\MovaFlag\\Settings", 0, KEY_READ, &hKey) == ERROR_SUCCESS)
+    {
+        // read the g_disable_ibeam value
+        RegQueryValueEx(hKey, "DisableIbeam", NULL, NULL, (LPBYTE)&disableIbeam, &size);
+        g_disable_ibeam = (BOOL)disableIbeam;
+
+        // read the g_disable_arrow value
+        RegQueryValueEx(hKey, "DisableArrow", NULL, NULL, (LPBYTE)&disableArrow, &size);
+        g_disable_arrow = (BOOL)disableArrow;
+
+        // Close the key
+        RegCloseKey(hKey);
+    }
+}
+
+
 int Main()
 {
     HANDLE mutex = CreateMutex(NULL, FALSE, "MovaFlag");
     if (GetLastError() == ERROR_ALREADY_EXISTS || GetLastError() == ERROR_ACCESS_DENIED)
         return 1;
 
-    g_hc_ibeam = CopyCursor(LoadCursor(NULL, IDC_IBEAM));
-    if (!g_hc_ibeam)
-        return 1;
-
-    g_hc_arrow = CopyCursor(LoadCursor(NULL, IDC_ARROW));
-    if (!g_hc_arrow)
-        return 1;
 
     g_instance = GetModuleHandle(NULL);
-    g_timer = SetTimer(NULL, g_timer, 200, UpdateTimer);
-    if (!g_timer)
-        return 1;
+
 
     WNDCLASS wc = { 0 };
     wc.lpfnWndProc = WindowProc;
@@ -118,7 +195,10 @@ int Main()
     if (!hWnd) return 1;
 
     g_hMenu = CreatePopupMenu();
+    AppendMenu(g_hMenu, MF_STRING | MF_UNCHECKED, ID_TRAY_DISABLE_ARROW, "Disable Arrow Flag");
+    AppendMenu(g_hMenu, MF_STRING | MF_UNCHECKED, ID_TRAY_DISABLE_IBEAM, "Disable IBeam Flag");
     AppendMenu(g_hMenu, MF_STRING, ID_TRAY_EXIT, "Exit");
+    //LoadMenuStrings(); // Load localized menu strings
 
     g_notifyIconData.cbSize = sizeof(NOTIFYICONDATA);
     g_notifyIconData.hWnd = hWnd;
@@ -129,6 +209,33 @@ int Main()
     StringCchCopy(g_notifyIconData.szTip, ARRAYSIZE(g_notifyIconData.szTip), "MovaFlag");
     Shell_NotifyIcon(NIM_ADD, &g_notifyIconData);
 
+
+    LoadSettings();
+    if (g_disable_ibeam)
+        CheckMenuItem(g_hMenu, ID_TRAY_DISABLE_IBEAM, MF_CHECKED);
+    else
+        CheckMenuItem(g_hMenu, ID_TRAY_DISABLE_IBEAM, MF_UNCHECKED);
+
+    if (g_disable_arrow)
+        CheckMenuItem(g_hMenu, ID_TRAY_DISABLE_ARROW, MF_CHECKED);
+    else
+        CheckMenuItem(g_hMenu, ID_TRAY_DISABLE_ARROW, MF_UNCHECKED);
+
+
+    g_hc_ibeam = CopyCursor(LoadCursor(NULL, IDC_IBEAM));
+    if (!g_hc_ibeam)
+        return 1;
+
+    g_hc_arrow = CopyCursor(LoadCursor(NULL, IDC_ARROW));
+    if (!g_hc_arrow)
+        return 1;
+
+
+    g_timer = SetTimer(NULL, g_timer, 200, UpdateTimer);
+    if (!g_timer)
+        return 1;
+
+    
     MSG msg;
     while (GetMessage(&msg, 0, 0, 0))
     {
@@ -136,11 +243,15 @@ int Main()
         DispatchMessage(&msg);
     }
 
-    SetSystemCursor(g_hc_ibeam, OCR_IBEAM);
-    SetSystemCursor(g_hc_arrow, OCR_NORMAL);
+
+    SetSystemCursor(CopyCursor(g_hc_ibeam), OCR_IBEAM);
+    SetSystemCursor(CopyCursor(g_hc_arrow), OCR_NORMAL);
 
     DestroyCursor(g_hc_ibeam);
     DestroyCursor(g_hc_arrow);
+
+    SaveSettings();
+
     return 0;
 }
 
